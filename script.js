54f1caf5836c82716210675ef2290f307b9e479f
mazdamike <mwpowell44@gmail.com>
2020-03-31 18:37:43 -0400

    saving work

--- script.js
+++ script.js
@@ -3,4 +3,23 @@ $(document).ready(function () {
     // Header date
     $("#currentDay").text(moment().format("dddd" + ", " + "MMMM Do"));
 
+   
+    var now = moment();
+    thisDay = moment().format("YYYY-MM-DD");
+    console.log(thisDay);
+
+    if (now.isBefore(thisDay + " " + "17:00:00")) {
+        console.log("color gray");
+    } else if (now.isAfter(thisDay + " " + "18:00:00")) {
+        console.log("color is green");
+    } else {
+        console.log("color is red");
+    }
+
+
+
+
+
+
+
 });
